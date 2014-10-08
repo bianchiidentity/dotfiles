de99b14387208b59c045b93915434353ef8f1e4b
kawasakikou <forte0000000000@yahoo.co.jp>
2014-10-08 22:31:55 +0900

    合ってるかわからんが適当に追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+chsh -s /usr/local/bin/zsh
+mkdir -p ~.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
