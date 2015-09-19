fdadf1fcffd5a074a5a6d099023b8064925a17e9
kawasakikou <forte0801@gmail.com>
2015-09-20 01:46:00 +0900

    完全にいらない

--- dotfilesLink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chsh -s /usr/local/bin/zsh
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
